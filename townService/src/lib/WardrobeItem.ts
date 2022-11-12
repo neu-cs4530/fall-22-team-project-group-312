9fc7153c18d87932d99a3a04d545dc7de5704e64
xiushak <30637265+xiushak@users.noreply.github.com>
2022-11-11 19:16:37 -0500

    added definitions of all items and wardrobe item type

--- townService/src/lib/WardrobeItem.ts
+++ townService/src/lib/WardrobeItem.ts
@@ -6,34 +6,25 @@ export type ItemCategory = 'skin' | 'eyes' | 'hair' | 'clothing' | 'accessory';
 /**
  * Represents a single item in a Wardrobe, either a skin color, eye color, hairstyle, clothing, or accessory.
  */
-export default class WardrobeItem {
-  /** The name of the WardrobeItem */
-  private readonly _name: string;
+export type WardrobeItem = {
+  name: string;
+  category: ItemCategory;
+  spriteLocation: string;
+};
 
-  /** The category of the item. */
-  private readonly _category: ItemCategory;
+const DEFAULT_ITEMS: WardrobeItem[] = [
+  { name: 'defualt hair', category: 'hair', spriteLocation: '' },
+  { name: 'defualt eyes', category: 'eyes', spriteLocation: '' },
+  { name: 'defualt clothing', category: 'clothing', spriteLocation: '' },
+  { name: 'no accessory', category: 'accessory', spriteLocation: '' },
+  { name: '0', category: 'skin', spriteLocation: '' },
+  { name: '1', category: 'skin', spriteLocation: '' },
+  { name: '2', category: 'skin', spriteLocation: '' },
+  { name: '3', category: 'skin', spriteLocation: '' },
+  { name: '4', category: 'skin', spriteLocation: '' },
+];
 
-  /** A string representing the location of the WardrobeItem sprite in the source code. */
-  private readonly _spriteLocation: string;
-
-  constructor(name: string, category: ItemCategory, spriteLocation: string) {
-    this._name = name;
-    this._category = category;
-    this._spriteLocation = spriteLocation;
-  }
-
-  // Returns the name of the WardrobeItem.
-  get name(): string {
-    return this._name;
-  }
-
-  // Returns the category of the WardrobeItem.
-  get category(): ItemCategory {
-    return this._category;
-  }
-
-  // Returns the sprite file location of the WardrobeItem.
-  get spriteLocation(): string {
-    return this._spriteLocation;
-  }
-}
+const UNLOCKABLE_ITEMS: WardrobeItem[] = [
+  { name: 'bald', category: 'hair', spriteLocation: '' },
+  { name: 'keqing', category: 'hair', spriteLocation: '' },
+];
