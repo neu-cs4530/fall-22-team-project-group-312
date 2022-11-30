6c611f8395761f28973354fce6009967817f28c6
Anna Ji <ji.a@northeastern.edu>
2022-11-29 21:02:33 -0500

    added title names for main button

--- frontend/src/components/VideoCall/VideoFrontend/components/Buttons/OpenWardrobeButton/OpenWardrobeButton.tsx
+++ frontend/src/components/VideoCall/VideoFrontend/components/Buttons/OpenWardrobeButton/OpenWardrobeButton.tsx
@@ -1,4 +1,5 @@
 import { useDisclosure } from '@chakra-ui/react';
+import { Typography } from '@material-ui/core';
 import MenuItem from '@material-ui/core/MenuItem';
 import React, { useCallback } from 'react';
 import useTownController from '../../../../../../hooks/useTownController';
@@ -15,7 +16,9 @@ export default function WardrobeButton() {
 
   return (
     <>
-      <MenuItem data-testid='openMenuButton' onClick={openWardrobe}></MenuItem>
+      <MenuItem data-testid='openMenuButton' onClick={openWardrobe}>
+        <Typography variant='body1'>Changing Room</Typography>
+      </MenuItem>
       <WardrobePanel
         isOpen={isOpen}
         onOpen={onOpen}
